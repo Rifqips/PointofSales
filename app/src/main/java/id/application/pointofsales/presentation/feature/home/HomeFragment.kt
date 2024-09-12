67f624bc7381dffc0de4a2436450330f93e560f7
rifqi.siliwangi <rifqipadisiliwangi@gmail.com>
2024-09-12 10:53:28 +0700

    fix : - bugs navigation rail

--- app/src/main/java/id/application/pointofsales/presentation/feature/home/HomeFragment.kt
+++ app/src/main/java/id/application/pointofsales/presentation/feature/home/HomeFragment.kt
@@ -1,22 +1,13 @@
 package id.application.pointofsales.presentation.feature.home
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import androidx.window.layout.WindowMetricsCalculator
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.navigationrail.NavigationRailView
 import id.application.pointofsales.R
-import id.application.pointofsales.databinding.DialogPrintBillsBinding
 import id.application.pointofsales.databinding.FragmentHomeBinding
 import id.application.pointofsales.presentation.viewmodel.VmApplication
 import id.application.pointofsales.utils.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class HomeFragment :
     BaseFragment<FragmentHomeBinding, VmApplication>(FragmentHomeBinding::inflate) {
@@ -33,106 +24,26 @@ class HomeFragment :
         val navHostFragment =
             childFragmentManager.findFragmentById(R.id.container_bottom_navigation) as? NavHostFragment
         val navController = navHostFragment?.navController
-        val metrics = context?.let {
-            WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(it)
-        }
-        val withDp = metrics?.bounds?.width()?.div(resources.displayMetrics.density)
 
-        with(binding) {
-            if (withDp != null) {
-                when {
-                    withDp < 600f -> {
-                        if (binding.bottomNavigation is BottomNavigationView && navController != null) {
-                            val bottomNav = binding.bottomNavigation as BottomNavigationView
-                            bottomNav.setupWithNavController(navController)
-                            bottomNav.setOnItemSelectedListener {
-                                when (it.itemId) {
-                                    R.id.menuCashier -> {
-                                        navController.navigate(R.id.cashierFragmentFragment)
-                                    }
-                                    R.id.menuProduct -> {
-                                        navController.navigate(R.id.productFragment)
-                                    }
-                                    R.id.menuSetting -> {
-                                        navController.navigate(R.id.settingFragment)
-                                    }
-                                }
-                                true
-                            }
-                        }
+        if (navController != null) {
+            binding.navigationRail.setupWithNavController(navController)
+            binding.navigationRail.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menuCashier -> {
+                        navController.navigate(R.id.cashierFragment)
                     }
-                    withDp < 840f -> {
-                        if (binding.bottomNavigation is NavigationRailView && navController != null) {
-                            val navRail = binding.bottomNavigation as NavigationRailView
-                            navRail.setupWithNavController(navController)
-                            navRail.setOnItemSelectedListener {
-                                when (it.itemId) {
-                                    R.id.menuCashier -> {
-                                        navController.navigate(R.id.cashierFragmentFragment)
-                                    }
-                                    R.id.menuProduct -> {
-                                        navController.navigate(R.id.productFragment)
-                                    }
-                                    R.id.menuSetting -> {
-                                        navController.navigate(R.id.settingFragment)
-                                    }
-                                }
-                                true
-                            }
-                        }
+                    R.id.menuProduct -> {
+                        navController.navigate(R.id.productFragment)
                     }
-                    else -> {
-                        if (binding.bottomNavigation is NavigationView && navController != null) {
-                            val bottomNav = binding.bottomNavigation as BottomNavigationView
-                            bottomNav.setupWithNavController(navController)
-                            bottomNav.setOnItemSelectedListener {
-                                when (it.itemId) {
-                                    R.id.menuCashier -> {
-                                        navController.navigate(R.id.cashierFragmentFragment)
-                                    }
-                                    R.id.menuProduct -> {
-                                        navController.navigate(R.id.productFragment)
-                                    }
-                                    R.id.menuSetting -> {
-                                        navController.navigate(R.id.settingFragment)
-                                    }
-                                }
-                                true
-                            }
-                        }
+                    R.id.menuSetting -> {
+                        navController.navigate(R.id.settingFragment)
                     }
                 }
+                true
             }
         }
     }
-
-
-    private var activeDialog: AlertDialog? = null
-
-    @SuppressLint("ClickableViewAccessibility")
-    private fun showDialogConfirmSaveData() {
-        activeDialog?.let { dialog ->
-            if (dialog.isShowing) {
-                dialog.dismiss()
-            }
-        }
-        val binding: DialogPrintBillsBinding = DialogPrintBillsBinding.inflate(layoutInflater)
-        val dialog = AlertDialog.Builder(requireContext(), 0).create()
-        dialog.apply {
-            setView(binding.root)
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            setCanceledOnTouchOutside(false)
-        }.show()
-        activeDialog = dialog
-        with(binding) {
-            ivClose.setOnClickListener {
-                dialog.dismiss()
-                activeDialog = null
-            }
-        }
-        binding.root.setOnTouchListener { _, _ ->
-            true
-        }
-    }
-
 }
+
+
+
--- app/src/main/java/id/application/pointofsales/presentation/feature/login/LoginFragment.kt
+++ app/src/main/java/id/application/pointofsales/presentation/feature/login/LoginFragment.kt
@@ -1,10 +1,5 @@
 package id.application.pointofsales.presentation.feature.login
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import id.application.pointofsales.R
@@ -13,7 +8,8 @@ import id.application.pointofsales.presentation.viewmodel.VmApplication
 import id.application.pointofsales.utils.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginFragment : BaseFragment<FragmentLoginBinding, VmApplication>(FragmentLoginBinding::inflate) {
+class LoginFragment :
+    BaseFragment<FragmentLoginBinding, VmApplication>(FragmentLoginBinding::inflate) {
 
     override val viewModel: VmApplication by viewModel()
 
