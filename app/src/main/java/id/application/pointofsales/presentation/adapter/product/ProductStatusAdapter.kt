e215f25074911c272ee4640dbfbb88e0db4824fd
rifqipadisiliwangi <Rifqipadi99@gmail.com>
2024-09-16 09:02:19 +0700

    feat : - slicing product status ref : color button search

---
+++ app/src/main/java/id/application/pointofsales/presentation/adapter/product/ProductStatusAdapter.kt
@@ -0,0 +1,59 @@
+package id.application.pointofsales.presentation.adapter.product
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import id.application.pointofsales.R
+import id.application.pointofsales.databinding.ItemProductStatusBinding
+
+
+class ProductStatusAdapter(
+    private val productList: List<ProductStatus>,
+    private val onClick: (ProductStatus) -> Unit
+) : RecyclerView.Adapter<ProductStatusAdapter.ProductViewHolder>() {
+
+    inner class ProductViewHolder(private val binding: ItemProductStatusBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(product: ProductStatus) {
+            binding.ivUrl.load(product.imageUrl)
+
+            binding.tvProduct.text = product.productName
+            binding.tvBouquetName.text = product.bouquetName
+            binding.tvDatetime.text = product.datetime
+            binding.tvHargaJual.text = product.salePrice
+            binding.tvStatus.text = product.status
+            binding.btnAddCart.setOnClickListener {
+                onClick(product)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val binding = ItemProductStatusBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ProductViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.bind(productList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+}
+
+data class ProductStatus(
+    val imageUrl: String,
+    val productName: String,
+    val bouquetName: String,
+    val status: String,
+    val datetime: String,
+    val salePrice: String
+)
+
--- app/src/main/java/id/application/pointofsales/presentation/feature/product/ProductFragment.kt
+++ app/src/main/java/id/application/pointofsales/presentation/feature/product/ProductFragment.kt
@@ -1,16 +1,71 @@
 package id.application.pointofsales.presentation.feature.product
 
+import androidx.recyclerview.widget.GridLayoutManager
 import id.application.pointofsales.databinding.FragmentProductBinding
+import id.application.pointofsales.presentation.adapter.history.HistoryAdapter
+import id.application.pointofsales.presentation.adapter.history.HistoryItem
+import id.application.pointofsales.presentation.adapter.product.ProductStatus
+import id.application.pointofsales.presentation.adapter.product.ProductStatusAdapter
 import id.application.pointofsales.presentation.viewmodel.VmApplication
 import id.application.pointofsales.utils.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ProductFragment : BaseFragment<FragmentProductBinding, VmApplication>(FragmentProductBinding::inflate) {
+class ProductFragment :
+    BaseFragment<FragmentProductBinding, VmApplication>(FragmentProductBinding::inflate) {
 
     override val viewModel: VmApplication by viewModel()
+    private lateinit var productAdapter: ProductStatusAdapter
 
-    override fun initView() {}
+    override fun initView() {
+        setupRecyclerView()
+    }
 
-    override fun initListener() {}
 
+    private fun setupRecyclerView() {
+        val productList = listOf(
+                ProductStatus(
+                imageUrl = "https://i.pinimg.com/736x/c7/26/40/c72640b4c7fc07895b392c98755a1164.jpg",
+                productName = "Bouquet",
+                bouquetName = "Rose Bouquet",
+                status = "Lunas",
+                datetime = "10-10-2024",
+                salePrice = "Rp.100.000,00"
+            ),
+                ProductStatus(
+                imageUrl = "https://i.pinimg.com/originals/d8/2a/cd/d82acd6891fc8cc98dfbd4db9604e53c.jpg",
+                productName = "Bouquet",
+                bouquetName = "Rose Bouquet",
+                status = "Belum Lunas",
+                datetime = "10-10-2024",
+                salePrice = "Rp.100.000,00"
+            ),
+                ProductStatus(
+                imageUrl = "https://i.pinimg.com/236x/bb/a6/4e/bba64e0a760b5ea6fe1b13ca34244467.jpg",
+                productName = "Bouquet",
+                bouquetName = "Rose Bouquet",
+                status = "Lunas",
+                datetime = "10-10-2024",
+                salePrice = "Rp.100.000,00"
+            ),
+                ProductStatus(
+                imageUrl = "https://i.pinimg.com/736x/98/84/94/9884942884bc2368767f87799b7526e8.jpg",
+                productName = "Bouquet",
+                bouquetName = "Rose Bouquet",
+                status = "Belum Lunas",
+                datetime = "10-10-2024",
+                salePrice = "Rp.100.000,00"
+            ),
+        )
+
+        productAdapter = ProductStatusAdapter(productList) { product ->
+        }
+        binding.rvProductStatus.adapter = productAdapter
+        val gridLayoutManager = GridLayoutManager(requireContext(), 3)
+        binding.rvProductStatus.layoutManager = gridLayoutManager
+    }
+
+
+    override fun initListener() {
+        // Set listeners for other views like search, filter etc.
+    }
 }
